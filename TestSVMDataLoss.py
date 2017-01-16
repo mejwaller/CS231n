dd27fc61d93d91b91fb651d2a38e0c35522da5c4
mejwaller <martin.waller@asglh-wl-08471.kingsway.asos.com>
2017-01-16 13:29:44 +0000

    Add tesst for unvertorised version on dev test set (fixed)

--- TestSVMDataLoss.py
+++ TestSVMDataLoss.py
@@ -7,14 +7,15 @@ class myTest(unittest.TestCase):
     #see http://cs231n.github.io/linear-classify/ 
     #(section Multiclass Support Vector Machine loss)  
     def setUp(self):
-        pp=preproc()
-        pp.preProcess()
+        self.pp=preproc()
+        self.pp.preProcess()
         xs=[13,-7,11,1]
         self.xtest=np.array(xs)
         #Ws=[[1,0,0,0],[0,1,0,0],[0,0,1,0]]
         #self.Wtest = np.array(Ws)
         self.Wtest = np.eye(3,4)
         self.thing=6
+        self.Wpreproc = np.eye(10,3073)*2
     
     #check our test Wx is correct   
     def testWdotx(self):
@@ -27,6 +28,14 @@ class myTest(unittest.TestCase):
         expected = 8
         self.failUnless(SVMDataLoss.L_i(self.xtest,0,self.Wtest,10)==8)  
         
+    def testunvectorizedL_ifortestset(self):#get loss for first image of preprocessor fixed dev set
+        print SVMDataLoss.L_i(self.pp.fixedX_dev[1],self.pp.fixedy_dev[1], self.Wpreproc)   
+        
+    def testunvectorisedL_iforFullTestSet(self):# get total loss               
+        score = 0;
+        for i in xrange(500):
+            score+=SVMDataLoss.L_i(self.pp.fixedX_dev[i], self.pp.fixedy_dev[i],self.Wpreproc)
+        print score
     
         
 if __name__ == '__main__': unittest.main()
