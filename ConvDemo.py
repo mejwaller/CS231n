46e7c608aee1ff2a2a714c04677d7ea395548d19
mejwaller <martin.waller@asglh-wl-08471.kingsway.asos.com>
2017-04-03 15:26:19 +0100

    padding and tests

--- ConvDemo.py
+++ ConvDemo.py
@@ -11,9 +11,25 @@ class net:
         self.K=nfilters
         self.IV=inimg
         
+        if(self.P>0):
+            self.padded = np.zeros((self.IV.shape[0]+2*self.P,self.IV.shape[1]+2*self.P,self.IV.shape[2]))
+            self.padded[self.P:self.padded.shape[0]-self.P,self.P:self.padded.shape[1]-self.P,:]=self.IV
+        else:
+            self.padded=self.IV
+            
+      
+    #should probably store filters and other params in dict - but this just to ensure O can get 
+    #demo workign, not design a fully funtioning convnet framework
     def setFilter(self,filter,num):#set numth filter to filter = throw FilterError if num-1 > K
         if num > self.K:
             raise FilterError
+            
+        if(num==0):
+            self.W0 = filter
+            
+        if(num==1):
+            self.W1=filter
+        
         
         
 class Error(Exception):#base class
--- TestConvDemo.py
+++ TestConvDemo.py
@@ -14,67 +14,67 @@ class ConvDemoTest(unittest.TestCase):
         self.S=2#convent neuron stride
         self.P=1#zero padding size
         self.K=2#number of filters
-        self.IV = np.asarray([
-                    [
+        self.IV=np.zeros((5,5,3))
+        self.IV[:,:,0]=np.asarray([
                         [1,1,1,1,0],
                         [2,2,0,0,1],
                         [2,1,2,0,1],
                         [0,1,1,1,1],
                         [1,2,1,1,0]
-                    ],
-                    [
+                    ])
+        self.IV[:,:,1]=np.asarray([
                         [1,2,0,0,2],
                         [2,0,2,1,2],
                         [1,1,0,0,0],
                         [1,0,1,2,2],
                         [2,0,2,0,1]
-                    ],
-                    [
+                    ])
+        self.IV[:,:,2]=np.asarray([
                         [1,0,0,0,2],
                         [1,0,2,2,2],
                         [1,1,0,2,1],
                         [0,1,2,1,0],
                         [0,1,0,1,1]
-                    ]
-                ])#convdemo input
-                
-        self.W0 = np.asarray([
-                [
+                    ])
+
+        self.W0=np.zeros((3,3,3))
+        self.W0[:,:,0] = np.asarray([
                     [0,0,0],
                     [-1,0,0],
                     [-1,0,-1]
-                ],
-                [
+                ])
+        self.W0[:,:,1]=np.asarray([
                     [1,0,1],
                     [-1,-1,1],
                     [-1,1,1]
-                ],
-                [
+                ])
+        self.W0[:,:,2]=np.asarray([
                     [1,1,0],
                     [1,1,1],
                     [1,-1,1]
-                ]
-            ])
-        self.W1 = np.asarray([
-                [
+                ])
+                
+        self.W1=np.zeros((3,3,3))
+        self.W1[:,:,0] = np.asarray([
                     [-1,-1,1],
                     [1,1,0],
                     [-1,0,-1]
-                ],
-                [
+                ])
+        self.W1[:,:,1]=np.asarray([
                     [1,-1,-1],
                     [-1,-1,-1],
                     [-1,1,1],
-                ],
-                [
+                ])
+        self.W1[:,:,0]=np.asarray([
                     [1,0,1],
                     [0,0,-1],
                     [1,1,-1]
-                ]
-            ])
+                ])
+
+        self.convnet= conv.net(self.W,self.H,self.D,self.F,self.S,self.P,self.K,self.IV)
                 
     def testConstruction(self):
-        convnet= conv.net(self.W,self.H,self.D,self.F,self.S,self.P,self.K,self.IV)
+        cvnet= conv.net(self.W,self.H,self.D,self.F,self.S,self.P,self.K,self.IV)
         expW = self.W
         expH = self.H
         expD = self.D
@@ -84,19 +84,35 @@ class ConvDemoTest(unittest.TestCase):
         expK = self.K
         expIV = self.IV
         
-        self.failUnless(convnet.W == expW)
-        self.failUnless(convnet.H == expH)
-        self.failUnless(convnet.D == expD)
-        self.failUnless(convnet.F == expF)
-        self.failUnless(convnet.S == expS)
-        self.failUnless(convnet.P == expP)
-        self.failUnless(convnet.K == expK)
-        self.failUnless(np.array_equal(convnet.IV,expIV))
+        self.failUnless(cvnet.W == expW)
+        self.failUnless(cvnet.H == expH)
+        self.failUnless(cvnet.D == expD)
+        self.failUnless(cvnet.F == expF)
+        self.failUnless(cvnet.S == expS)
+        self.failUnless(cvnet.P == expP)
+        self.failUnless(cvnet.K == expK)
+        self.failUnless(np.array_equal(cvnet.IV,expIV))
         
     def testSetWrongFilter(self):
-        convnet= conv.net(self.W,self.H,self.D,self.F,self.S,self.P,self.K,self.IV)
         with self.assertRaises(conv.FilterError):
-            convnet.setFilter(self.W0,3)
+            self.convnet.setFilter(self.W0,3)
+            
+    def testSetFilter(self):        
+        self.convnet.setFilter(self.W0,0)
+        self.convnet.setFilter(self.W1,1)
+        self.failUnless(np.array_equal(self.W0,self.convnet.W0))
+        self.failUnless(np.array_equal(self.W1,self.convnet.W1))
+        
+    def testPadding(self):
+        self.failUnless(self.convnet.padded.shape[0]==7)
+        self.failUnless(self.convnet.padded.shape[1]==7)
+        self.failUnless(self.convnet.padded.shape[2]==3)
+        
+    def testPaddedData(self):
+        self.failUnless(np.array_equal(self.convnet.IV,self.convnet.padded[self.convnet.P:self.convnet.padded.shape[0]-self.convnet.P,self.convnet.P:self.convnet.padded.shape[1]-self.convnet.P,:]))
+        
+        
+        
         
         
         
